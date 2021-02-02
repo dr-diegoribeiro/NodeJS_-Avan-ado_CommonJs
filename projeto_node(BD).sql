edaed008d7785d3e5824fd6bace7d44a17367eac
dr-diegoribeiro <72672238+dr-diegoribeiro@users.noreply.github.com>
2021-02-02 13:37:02 -0300

    NodeJS_Avançado_CommonJs

---
+++ projeto_node(BD).sql
@@ -0,0 +1,62 @@
+CREATE DATABASE  IF NOT EXISTS `projeto_node` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `projeto_node`;
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projeto_node
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `conteudo`
+--
+
+DROP TABLE IF EXISTS `conteudo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conteudo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categoria` varchar(45) DEFAULT NULL,
+  `conteudo` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conteudo`
+--
+
+LOCK TABLES `conteudo` WRITE;
+/*!40000 ALTER TABLE `conteudo` DISABLE KEYS */;
+INSERT INTO `conteudo` VALUES (1,'JavaScript','NodeJs'),(2,'Front','Front'),(3,'Back','Hello'),(4,'Back','PHP'),(5,'Front','CSS'),(6,'Back','Python');
+/*!40000 ALTER TABLE `conteudo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'projeto_node'
+--
+
+--
+-- Dumping routines for database 'projeto_node'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-27 11:17:31
